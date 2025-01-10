16387803044c46b721331bc09d8b3dd2a29ba9a8
Sipioteo <matteo@sipio.it>
2025-01-10 15:50:45 +0100

    chore(deps): Update device_info_plus to 11.2.0, flutter_secure_storage to 1.2.2, and firebase_core to 3.10.0

--- android/app/src/main/kotlin/it/mensa/app/MainActivity.kt
+++ android/app/src/main/kotlin/it/mensa/app/MainActivity.kt
@@ -1,5 +1,6 @@
 package it.mensa.app
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
