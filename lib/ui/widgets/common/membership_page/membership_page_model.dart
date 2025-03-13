02a817eb94d169932658cae46bf2d6a744eff215
Sipioteo <matteo@sipio.it>
2025-03-13 13:21:36 +0100

    [RELEASE]

--- lib/ui/widgets/common/membership_page/membership_page_model.dart
+++ lib/ui/widgets/common/membership_page/membership_page_model.dart
@@ -35,25 +35,20 @@ class MembershipPageModel extends MasterModel {
       rebuildUi();
     });
 
-    Api()
-        .pb
-        .collection("user_notifications")
-        .subscribe(
-          '*',
-          updateUserNotificationCount,
-        )
-        .then((value) {
+    Api().pb.collection("user_notifications").subscribe('*', updateUserNotificationCount).then((value) {
       unsubscribeFuncForNotifications = value;
+    });
+    Api().pb.collection("user_notifications").getFullList(
+      filter: "seen = ''",
+    ).then((value) {
+      unseenNotifications = value.length;
+      rebuildUi();
     });
 
     SharedPreferences.getInstance().then((prefs) async {
       favsAddons.clear();
       if (!allowTestMakerAddon()) {
-        await prefs.setStringList(
-            "addons_fav",
-            (prefs.getStringList("addons_fav") ?? [])
-              ..removeWhere(
-                  (element) => element.startsWith("INTERNAL:testmakers")));
+        await prefs.setStringList("addons_fav", (prefs.getStringList("addons_fav") ?? [])..removeWhere((element) => element.startsWith("INTERNAL:testmakers")));
       }
       favsAddons.addAll(prefs.getStringList("addons_fav") ?? []);
       Api().getAddons().then((value) {
@@ -62,8 +57,7 @@ class MembershipPageModel extends MasterModel {
         List<String> toRemove = [];
         for (var favsAddon in favsAddons) {
           if (favsAddon.startsWith("EXTERNAL:")) {
-            if (!value
-                .any((element) => "EXTERNAL:${element.id}" == favsAddon)) {
+            if (!value.any((element) => "EXTERNAL:${element.id}" == favsAddon)) {
               toRemove.add(favsAddon);
             }
           }
@@ -121,8 +115,7 @@ class MembershipPageModel extends MasterModel {
 
   Function() openExternalAddon(AddonModel addon) {
     return () {
-      navigationService.navigateToExternalAddonWebviewView(
-          addonID: addon.id, addonURL: addon.url);
+      navigationService.navigateToExternalAddonWebviewView(addonID: addon.id, addonURL: addon.url);
     };
   }
 
