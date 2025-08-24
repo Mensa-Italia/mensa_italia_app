65dc4c0d5981d104edd266e74e54fe1b9bbb3424
Sipioteo <matteo@sipio.it>
2025-08-24 18:08:33 +0200

    [RELEASE]

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -55,7 +55,12 @@ android {
 
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
             signingConfig = signingConfigs.getByName("release")
         }
     }
