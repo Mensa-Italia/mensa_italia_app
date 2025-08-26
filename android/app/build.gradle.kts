759cfca4270d9bf8c6f8c3ca34ddf1452a12de76
Sipioteo <matteo@sipio.it>
2025-08-26 18:48:04 +0200

    [RELEASE]

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
     defaultConfig {
         applicationId = "it.mensa.app"
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
